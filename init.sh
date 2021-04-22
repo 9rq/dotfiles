a082726f291e51fc836591d18ceab84d83a481e6
len <l.yoshida.132@gmail.com>
2021-04-22 16:47:10 +0900

    check if link or unlink

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
-# homeにシンボリックリンクを貼り、環境パスを追加する
-ln -s ${HOME}/.config/dotfiles/zsh/.zshenv ${HOME}/.zshenv
-#unlink ${HOME}/.zshenv
+install=true
+if [install]; then
+    # homeにシンボリックリンクを貼り、環境パスを追加する
+    ln -s ${HOME}/.config/dotfiles/zsh/.zshenv ${HOME}/.zshenv
+    ln -s ${HOME}/.config/dotfiles/nvim/* ${HOME}/.config/nvim
+else
+    unlink ${HOME}/.zshenv
+    unlink ${HOME}/.config/dotfiles/nvim/*
+fi

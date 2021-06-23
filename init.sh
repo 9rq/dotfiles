b1afb23f5e303d418187dd29c5882d75f7cb93e5
len <l.yoshida.132@gmail.com>
2021-06-23 18:49:45 +0900

    add conditions

--- init.sh
+++ init.sh
@@ -1,8 +1,16 @@
 install=true
+zsh=true
+nvim=true
+
 if [install]; then
     # homeにシンボリックリンクを貼り、環境パスを追加する
-    ln -s ${HOME}/.config/dotfiles/zsh/.zshenv ${HOME}/.zshenv
-    ln -s ${HOME}/.config/dotfiles/nvim/* ${HOME}/.config/nvim
+    if [zsh]; then
+        ln -s ${HOME}/.config/dotfiles/zsh/.zshenv ${HOME}/.zshenv
+    fi
+
+    if [nvim]; then
+        ln -s ${HOME}/.config/dotfiles/nvim/* ${HOME}/.config/nvim
+    fi
 else
     unlink ${HOME}/.zshenv
     unlink ${HOME}/.config/dotfiles/nvim/*

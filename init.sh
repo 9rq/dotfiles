c0c6384de89fff3c7824d07e3a78978d025cc1f9
len <l.yoshida.132@gmail.com>
2021-07-06 22:43:28 +0900

    add tmux

--- init.sh
+++ init.sh
@@ -7,14 +7,18 @@ if $install; then
     # homeにシンボリックリンクを貼り、環境パスを追加する
     if $zsh; then
         ln -s ${HOME}/.config/dotfiles/zsh/.zshenv ${HOME}/.zshenv
+        ln -s ${HOME}/.config/dotfiles/zsh/.zshrc ${HOME}/.zshrc
     fi
 
     if $nvim; then
         ln -s ${HOME}/.config/dotfiles/nvim/* ${HOME}/.config/nvim
     fi
+
     if $tmux; then
         ln -s ${HOME}/.config/dotfiles/tmux/tmux.conf ${HOME}/.tmux.conf
 else
     unlink ${HOME}/.zshenv
+    unlink ${HOME}/.zshrc
     unlink ${HOME}/.config/dotfiles/nvim/*
+    unlink ${HOME}/.tmux.conf
 fi

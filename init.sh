754c5db73a327a0a1c5f2aea1e6aa5f90d3d4e1d
len <l.yoshida.132@gmail.com>
2021-07-06 22:39:10 +0900

    add jj keybind

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 install=true
 zsh=true
 nvim=true
+tmux=true
 
 if $install; then
     # homeにシンボリックリンクを貼り、環境パスを追加する
@@ -11,6 +12,8 @@ if $install; then
     if $nvim; then
         ln -s ${HOME}/.config/dotfiles/nvim/* ${HOME}/.config/nvim
     fi
+    if $tmux; then
+        ln -s ${HOME}/.config/dotfiles/tmux/tmux.conf ${HOME}/.tmux.conf
 else
     unlink ${HOME}/.zshenv
     unlink ${HOME}/.config/dotfiles/nvim/*
